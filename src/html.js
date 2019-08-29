925cf0a295476864b2df39b90d9d556a66087046
jenlmills <54607591+jenlmills@users.noreply.github.com>
2019-08-28 21:43:51 -0400

    remove favicon

--- src/html.js
+++ src/html.js
@@ -14,7 +14,6 @@ export default class HTML extends React.Component {
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
           {this.props.headComponents}
-         // <link rel="shortcut icon" href={favicon} />
         </head>
         <body {...this.props.bodyAttributes}>
           {this.props.preBodyComponents}
