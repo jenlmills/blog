c2d1fd274c05a2a97a7f76a56c6d1e17e2d838ae
jenlmills <54607591+jenlmills@users.noreply.github.com>
2019-08-28 20:34:09 -0400

    modify links

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
   title: 'Jenn''s Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
+  author: 'Jennifer Laughter Mills', // Required
   description: 'Full-stack Web Developer',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    website: 'https://jenniferlaughter.com',
+    github: 'https://github.com/jenlmills',
+    twitter: 'https://twitter.com/channelmills',
+    linkedin: 'https://www.linkedin.com/in/jenlmills',
   },
   pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  siteUrl: 'https://jenniferlaughter.com/',
 };
