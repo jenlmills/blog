b41367be4d77307c16fbcea5ef4e8d2a3280ec60
jenlmills <54607591+jenlmills@users.noreply.github.com>
2019-09-01 19:11:55 -0400

    remove header image

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -15,9 +15,10 @@ function Header({ config }) {
   return (
     <Container>
       <Wrapper>
+        /* jen commented out - remove <> image
         {userConfig.showHeaderImage && (
           <HeaderImage/>
-        )}
+        )} */
         <H1><Link to="/">{author}</Link></H1>
         <P>{description}</P>
         {social &&
